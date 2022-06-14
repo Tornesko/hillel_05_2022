1bc516fd68ef57868039ef2dffd7a7481869c979
Tornesko <konstantinborovenskij@gmail.com>
2022-06-14 13:21:39 +0300

    commit

--- lesson_4/home_work_4/hw4.py
+++ lesson_4/home_work_4/hw4.py
@@ -2,13 +2,14 @@ FILENAME = 'rockyou.txt'
 
 results = []
 with open(FILENAME, encoding="latin-1") as f:
-    a = input('Do you want to add lines with \"user\" to list?\nYes-y\nNo-n\n')
-    if a == 'y' or a == 'Y':
-        for cnt, line in enumerate(f.readlines()):
-            if "user" in line:
+
+     for cnt, line in enumerate(f.readlines()):
+        if "user" in line:
+            a = input('Line with \"user\" just found, add this line to list?\nYes-y\nNo-n\n')
+            if a == 'y' or a == 'Y':
                 results.append(line.replace("\n", ""))
-    elif a == 'n' or a == 'N':
-        pass
-    else:
-        print("you entered invalid value")
+            elif a == 'n' or a == 'N':
+                pass
+            else:
+                print("you entered invalid value")
 print(results, '\n', len(results))
