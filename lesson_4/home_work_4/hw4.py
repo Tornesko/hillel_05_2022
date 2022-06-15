c952614b0676a055e61e4932ba8707c2abb02ff5
Tornesko <konstantinborovenskij@gmail.com>
2022-06-15 11:24:12 +0300

    commit

--- lesson_4/home_work_4/hw4.py
+++ lesson_4/home_work_4/hw4.py
@@ -4,10 +4,8 @@ results = []
 with open(FILENAME, encoding="latin-1") as f:
     for cnt, line in enumerate(f.readlines()):
         if "user" in line:
-            a = input(
-                'Line with "user" just found,'
-                " add this line to list?\nYes-y\nNo-n\n"
-            )
+            print('Line with "user" just found,', end="")
+            a = input(" add this line to list?\nYes-y\nNo-n\n")
             if a == "y" or a == "Y":
                 results.append(line.replace("\n", ""))
             elif a == "n" or a == "N":
