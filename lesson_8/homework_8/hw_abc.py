cb98a30579990bd85db8401bee0a0fac3f15bd95
Tornesko <konstantinborovenskij@gmail.com>
2022-06-20 14:23:00 +0300

    commit

---
+++ lesson_8/homework_8/hw_abc.py
@@ -0,0 +1,31 @@
+﻿from abc import ABC, abstractclassmethod
+from random import choice
+
+
+class Shape(ABC):
+    @abstractclassmethod
+    def draw(self):
+        pass
+
+
+class Rectangle(Shape):
+    def draw(self):
+        print("----", "|  |", "----", sep="\n")
+
+
+class Circle(Shape):
+    def draw(self):
+        print(" -- ", "-  -", " -- ", sep="\n")
+
+
+def get_shape(cls) -> Shape:
+    options: list[Shape] = [Rectangle(), Circle()]
+    return choice(options).draw()
+
+
+def main():
+    get_shape(Shape)
+
+
+if __name__ == "__main__":
+    main()
